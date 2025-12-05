abe2ba9cb6c5ffe5b5e89535d5ed8e576841428d
SPUCEEE <egle20161130@gmail.com>
2025-12-05 12:12:55 +0100

    började designa main sidan.

---
+++ DeluxQUIZ/header.php
@@ -0,0 +1,5 @@
+<div id="header">
+<h1>LOGO</h1>
+<h1>O</h1>
+</div>
+<hr>
---
+++ DeluxQUIZ/main.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+    include("header.php");
+    ?>
+    <div>
+        <h1 class="title">QUIZES</h1>
+        <input type="text" name="search" id="search">
+        <div class="card" style="width: 12rem;">
+            <img class="card-img-top" src="placeholder.png" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
